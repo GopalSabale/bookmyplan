45f2525d755a873d7769f3f0cd4c8df9d5e19d27
GopalSabale <gopalsabale4@gmail.com>
2026-01-23 18:30:07 +0530

    codefactor

---
+++ src/main/java/com/example/mybookplan/flight.java
@@ -0,0 +1,13 @@
+package com.example.mybookplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+
+    @GetMapping("/flight")
+    public String getData() {
+        return "Hello Welcome to BookMyPlan, Please book  flight at 10% discount";
+    }
+}

531d5d6f064b8203767e926509195e0ebb2be623
GopalSabale <gopalsabale4@gmail.com>
2026-01-27 22:48:31 +0530

    adding flight discuont

--- src/main/java/com/example/mybookplan/flight.java
+++ src/main/java/com/example/mybookplan/flight.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flight {
+public class fighter_jet {
 
-    @GetMapping("/flight")
+    @GetMapping("/fighter_jet")
     public String getData() {
 <<<<<<< HEAD
-        return "Hello Welcome to BookMyPlan, Please book  flight at 20% discount";
+        return "Hello Welcome to BookMyPlan, Please book  fighter_jet at 20% discount";
 =======
-        return "Hello Welcome to BookMyPlan, Please book  flight at 30% discount";
+        return "Hello Welcome to BookMyPlan, Please book  fighter_jet at 30% discount";
 >>>>>>> 5bfbfb52287cbd936f52b40fb01defb6747624b8
     }
 }

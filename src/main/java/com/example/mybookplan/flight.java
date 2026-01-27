d4c5b2f1fe2fe75551d27b1576c6e8db2b22e97d
GopalSabale <122138380+GopalSabale@users.noreply.github.com>
2026-01-27 22:45:51 +0530

    Update flight.java

--- src/main/java/com/example/mybookplan/flight.java
+++ src/main/java/com/example/mybookplan/flight.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flight {
+public class fligher {
 
-    @GetMapping("/flight")
+    @GetMapping("/fighter")
     public String getData() {
 <<<<<<< HEAD
-        return "Hello Welcome to BookMyPlan, Please book  flight at 20% discount";
+        return "Hello Welcome to BookMyPlan, Please book  fighter at 20% discount";
 =======
-        return "Hello Welcome to BookMyPlan, Please book  flight at 30% discount";
+        return "Hello Welcome to BookMyPlan, Please book  fighter at 30% discount";
 >>>>>>> 5bfbfb52287cbd936f52b40fb01defb6747624b8
     }
 }
